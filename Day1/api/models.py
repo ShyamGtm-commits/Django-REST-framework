7f63da26b22deb57245e18dd87d6d00d86057476
ShyamGtm-commits <gautamshyam595@gmail.com>
2025-12-16 16:36:45 +0545

    refined and added the user_model to add in the settings.py

--- Day1/api/models.py
+++ Day1/api/models.py
@@ -22,23 +22,23 @@ class Product(models.Model):
     
 class Order(models.Model):
     class StatusChoices(models.TextChoices):
-        PENDING = 'pending'
-        CONFIRMED = 'confirmed'
-        CANCELLED = 'cancelled'
+        PENDING = 'Pending'
+        CONFIRMED = 'Confirmed'
+        CANCELLED = 'Cancelled'
 
-        order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-        user = models.ForeignKey(User, on_delete=models.CASCADE)
-        created_at = models.DateTimeField(auto_now_add=True)
-        status = models.CharField(
-            max_length=10, 
-            choices=StatusChoices.choices
-            default=StatusChoices.PENDING
-            )
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10, 
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+        )
         
-        products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
+    products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
 
-        def __str__(self):
-            return f"Order {self.order_id } by {self.user.username}"
+    def __str__(self):
+        return f"Order {self.order_id } by {self.user.username}"
         
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
