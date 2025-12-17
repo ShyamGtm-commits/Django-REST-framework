7474effc18daa14da9b65cad343c1c439bd06c22
ShyamGtm-commits <gautamshyam595@gmail.com>
2025-12-17 12:47:07 +0545

    wrote the view function to return serialized data (Json data)

--- Day1/api/views.py
+++ Day1/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from api.serializers import ProductSerializer
+from api.models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({
+        'data': serializer.data 
+    })
