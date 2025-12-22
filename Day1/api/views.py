ff05d169493382136f680b326172f4eab40ef9e8
ShyamGtm-commits <gautamshyam595@gmail.com>
2025-12-22 13:49:11 +0545

    JWT Authentication used and learned the way of the use of the rest client to api testing

--- Day1/api/views.py
+++ Day1/api/views.py
@@ -6,7 +6,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny
+    )
 from rest_framework.views import APIView
 
 
@@ -27,7 +31,11 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 # @api_view(['GET'])
